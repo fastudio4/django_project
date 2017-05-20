8cf44e386e223db40571d09a6bac527b79c33b7a
fastudio4 <fastudio4@gmail.com>
2017-05-20 22:26:19 +0300

    works urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.template, name='template'),
-    # url(r'^\w*$', views.home, name='home')
-    # url(r'^(?P<title_pro>\w+)/$', views.product, name='product'),
+    url(r'^$', views.home, name='home'),
+    url(r'^blog/$', views.blog, name='blog'),
+    url(r'^blog/(?P<pk>\w+)/$', views.article, name='article'),
+    url(r'^contacts/$', views.contacts, name='contacts')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from mysite.models import CategoryManufactures, CategoryCatalog # for base
 from mysite.models import CategoryArticle, Article # article content
 from mysite.models import Products # catalog products
 
-def template(request):
-    cat_pro = CategoryCatalog.objects.order_by('title_cat')
-    home_page = Article.objects.all()
-    return render(request, 'mysite/template.html', {
+def home(request):
+    cat_pro = CategoryCatalog.objects.order_by('pk')
+    home_page = Article.objects.filter(category_article=1).order_by('create_article')
+    return render(request, 'mysite/home.html', {
         'cat_pro': cat_pro,
         'home_page': home_page
     })
 
+def blog(request):
+    blog_page = Article.objects.filter(category_article=2).order_by('create_article')
+    articles = Article.objects.filter(category_article=2).order_by('create_article')[:5]
+    cat_pro = CategoryCatalog.objects.order_by('pk')
+    return render(request, 'mysite/blog.html', {
+        'blog_page': blog_page,
+        'articles': articles,
+        'cat_pro': cat_pro
+    })
+
+def article(request, pk):
+    article_one = get_object_or_404(Article, pk=pk)
+    articles = Article.objects.filter(category_article=2).order_by('create_article')[:5]
+    cat_pro = CategoryCatalog.objects.order_by('pk')
+    return render(request, 'mysite/article.html', {
+        'article_one': article_one,
+        'articles': articles,
+        'cat_pro': cat_pro
+    })
+
+def contacts(request):
+    contact_page = Article.objects.filter(category_article=3)
+    cat_pro = CategoryCatalog.objects.order_by('pk')
+    return render(request, 'mysite/contacts.html', {
+        'contact_page': contact_page,
+        'cat_pro': cat_pro
+    })

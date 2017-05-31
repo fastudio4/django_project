6103b8bdd967d17b576ed878be13e0d403c4d820
fastudio4 <fastudio4@gmail.com>
2017-05-31 18:34:06 +0300

    pagination in blog

--- base/settings.py
+++ base/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9a6u6v!o1rvafljh9y12yr=knu!g-oc8j)_jsjv0m(-p$uih-l'
+SECRET_KEY = con.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
